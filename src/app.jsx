ad6d5945d04f9239a2343372f457a0165cb269d7
GrypenBrigade <robloxbullcrap@gmail.com>
2026-02-28 21:37:17 +0800

    login page

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
 import './app.css';
+import Login from "./login";
 
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <div className="text-3xl font-bold underline flex items-center justify-center h-screen">
+            <Login />   
         </div>
     );
 }
+
+export default App;
---
+++ src/login.jsx
@@ -0,0 +1,30 @@
+function Login() {
+    return (
+        <div className="Login back">
+            <div className="text-3xl font-bold underline flex items-center justify-center h-screen">
+                <div className="flex flex-col items-center justify-center">
+                    <form className="bg-white p-6 rounded shadow-md">
+                        <div>
+                            <h1 className="text-4xl font-bold mb-4">Login</h1>
+                        </div>
+                        
+                        <div>
+                            <input type="text" placeholder="Username" className="mb-2 p-2 border border-gray-300 rounded" />
+                        </div>
+
+                        <div>
+                            <input type="password" placeholder="Password" className="mb-4 p-2 border border-gray-300 rounded" />
+                        </div>
+
+                        <div>
+                            <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
