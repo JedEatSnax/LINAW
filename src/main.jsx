96c683bec008eef33e7de7a544fbd9e263b2de0a
GrypenBrigade <robloxbullcrap@gmail.com>
2026-02-28 21:37:17 +0800

    login page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import react from "react";
 import reactDom from "react-dom/client";
 import App from "./app.jsx";
 import { BrowserRouter } from "react-router";
+import React from "react";
 
 const root = reactDom.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <React.StrictMode>  
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
