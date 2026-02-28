3e81bc7afd6fe533c5721dcff1edb1e4f583e04c
GrypenBrigade <robloxbullcrap@gmail.com>
2026-02-28 20:58:43 +0800

    Added dependences: Vite, tailwindcss, react-router, autoprefixer. Login screen in progress

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import './app.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
