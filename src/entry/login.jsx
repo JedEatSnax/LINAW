bffbb4a73b390e65a81e66b8d4400697556bfd6a
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-01 21:16:13 +0800

    Progress folder, file restructuring

--- src/entry/login.jsx
+++ src/entry/login.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function Login() {
     return (
-        <div className="Login back">
-            <div className="text-3xl font-bold underline flex items-center justify-center h-screen">
+        <div className="Login">
+            <div className="text-3xl font-bold flex items-center justify-center h-screen">
                 <div className="flex flex-col items-center justify-center">
                     <form className="bg-white p-6 rounded shadow-md">
                         <div>
@@ -9,15 +11,15 @@ function Login() {
                         </div>
                         
                         <div>
-                            <input type="text" placeholder="Username" className="mb-2 p-2 border border-gray-300 rounded" />
+                            <input type="text" placeholder="Username" className="mb-2 p-2 border border-gray-300 rounded" id="username"/>
                         </div>
 
                         <div>
-                            <input type="password" placeholder="Password" className="mb-4 p-2 border border-gray-300 rounded" />
+                            <input type="password" placeholder="Password" className="mb-4 p-2 border border-gray-300 rounded" id="password" />
                         </div>
 
                         <div>
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+                            <button className="bg-blue-500 text-white px-4 py-2 rounded" id="loginButton">Login</button>
                         </div>
 
                         <div className="mt-4">
@@ -25,7 +27,7 @@ function Login() {
                         </div>
 
                         <div>
-                            <a href="/register" className="text-blue-500">Don't have an account?</a>
+                            <span className="text-gray-500">Don't have an account? <Link to ="register" className="text-blue-500">Register here</Link> </span>
                         </div>
                         
                     </form>
