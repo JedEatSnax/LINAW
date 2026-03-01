3f4f676cc48ffd331d8043d11fcfbfe93aad086e
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-01 21:16:13 +0800

    Progress folder, file restructuring

---
+++ progress/readme.md
@@ -0,0 +1,10 @@
+# Progressos
+
+## Done
+- Install necessary dependencies
+- Login page
+- Progress folder and tangible images for reference
+
+## Todo
+- Figure out centering ``login.jsx`` and ``register.jsx`` without making the ``app.jsx`` do the centering
+- Make sidebar component
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './app.css';
-import Login from "./login";
+import Login from "./entry/login.jsx";
+import Register from "./entry/register.jsx";
 
 function App() {
     return (
-        <div className="text-3xl font-bold underline flex items-center justify-center h-screen" style={{"backgroundImage": "url(/assets/github-private-email.png)"}}>
-            <Login />   
+        <div className="text-3xl font-bold flex items-center justify-center h-screen" style={{"backgroundImage": "url(/assets/github-private-email.png)"}}>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
         </div>
     );
 }
