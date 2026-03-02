e42ef343d90d473f11ca716e91ba3167d0ec9f6a
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-03 00:37:02 +0800

    Login and register tweaks

--- src/client/entry/login.tsx
+++ src/client/entry/login.tsx
@@ -1,33 +1,29 @@
 import { Link } from "react-router-dom";
 function Login() {
     return (
-            <div className="text-3xl font-bold flex items-center justify-center h-screen">
+            <div className="text-3xl flex items-center justify-center h-screen">
                 <div className="flex flex-col items-center justify-center">
-                    <form className="bg-gray-800 font-mono p-4 rounded shadow-md">
-                        <div>
-                            <h1 className="text-4xl font-bold mb-4">Login</h1>
+                    <form className="bg-gray-800 font-mono p-4 rounded shadow-md w-80">
+                        <div className="flex items-center justify-center">
+                            <h1 className="text-2xl font-bold mb-4 text-amber-400">Login</h1>
                         </div>
                         
                         <div>
-                            <input type="text" placeholder="Username" className="mb-2 p-2 border border-gray-300 rounded" id="username"/>
+                            <input type="text" placeholder="Username" className="text-lg mb-2 p-2 border border-gray-300 rounded text-gray-400 w-full" id="username"/>
                         </div>
 
                         <div>
-                            <input type="password" placeholder="Password" className="mb-4 p-2 border border-gray-300 rounded" id="password" />
+                            <input type="password" placeholder="Password" className="text-lg mb-2 p-2 border border-gray-300 rounded text-gray-400 w-full" id="password" />
                         </div>
 
                         <div>
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded" id="loginButton">Login</button>
+                            <button className="bg-amber-600 text-white text-lg px-4 py-2 rounded w-full" id="loginButton">Login</button>
                         </div>
 
-                        <div className="mt-4">
-                            <a href="#" className="text-blue-500">Forgot password?</a>
+                        <div className="flex flex-col items-center justify-center mt-1">
+                            <a href="#" className="text-amber-300 text-sm">Forgot password?</a> 
+                            <span className="text-gray-300 text-sm">Don't have an account? <Link to="/register" className="text-amber-300 text-sm">Register here</Link> </span>
                         </div>
-
-                        <div>
-                            <span className="text-gray-500">Don't have an account? <Link to="/register" className="text-blue-500">Register here</Link> </span>
-                        </div>
-
                     </form>
                 </div>
             </div>
--- src/client/entry/register.tsx
+++ src/client/entry/register.tsx
@@ -3,35 +3,37 @@ import { Link } from "react-router-dom";
 
 function Register() {
     return (
-            <div className="text-3xl font-bold flex items-center justify-center h-screen">
+            <div className="text-3xl flex items-center justify-center h-screen">
                 <div className="flex flex-col items-center justify-center">
-                    <form className="bg-white p-6 rounded shadow-md">
+                    <form className="bg-gray-800 font-mono p-4 rounded shadow-md w-80">
                         <div>
                             <form>
-                                <h1 className="text-4xl font-bold mb-4 p-2">Register</h1>
-                                
-                                <div>
-                                    <input type="text" placeholder="Username" className="mb-2 p-2 border border-gray-300 rounded" id="username"/>
+                                <div className="flex items-center justify-center">
+                                    <h1 className="text-2xl text-amber-400 font-bold mb-4 p-2">Register</h1>
                                 </div>
                                 
                                 <div>
-                                    <input type="email" placeholder="Email" className="mb-2 p-2 border border-gray-300 rounded" id="email"/>
+                                    <input type="text" placeholder="Username" className="text-lg mb-2 p-2 border border-gray-300 text-gray-400 rounded w-full" id="username"/>
+                                </div>
+                                
+                                <div>
+                                    <input type="email" placeholder="Email" className="text-lg mb-2 p-2 border border-gray-300 text-gray-400 rounded w-full" id="email"/>
                                 </div>
 
                                 <div>
-                                    <input type="password" placeholder="Password" className="mb-2 p-2 border border-gray-300 rounded" id="password"/>
+                                    <input type="password" placeholder="Password" className="text-lg mb-2 p-2 border border-gray-300 text-gray-400 rounded w-full" id="password"/>
                                 </div>
 
                                 <div>
-                                    <input type="password" placeholder="Confirm Password" className="mb-4 p-2 border border-gray-300 rounded" id="confirmPassword"/>
+                                    <input type="password" placeholder="Confirm Password" className="text-lg mb-2 p-2 border border-gray-300 text-gray-400 rounded w-full" id="confirmPassword"/>
                                 </div>
 
-                                <div className="mb-4">
-                                    <button className="bg-green-500 text-white px-4 py-2 rounded" id="registerButton">Register</button>
+                                <div className="mb-2 flex items-center justify-center">
+                                    <button className="bg-amber-600 text-white text-lg px-4 py-2 rounded w-full" id="registerButton">Register</button>
                                 </div>
 
-                                <div>
-                                    <Link to="/login" className="text-blue-500">Already have an account?</Link>
+                                <div className="flex flex-col items-center justify-center mt-1">
+                                    <Link to="/login" className="text-amber-300 text-sm">Already have an account?</Link>
                                 </div>
                             </form>
                         </div>
