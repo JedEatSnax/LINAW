4929268995afafa070480402fa7b6797caa3624d
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-03 01:07:46 +0800

    tweak

--- src/client/main/home.tsx
+++ src/client/main/home.tsx
@@ -5,12 +5,12 @@ function Home() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold mb-4 text-amber-500">Welcome to the Home Page!</h1>
+                <h1 className="text-4xl font-bold mb-4 text-amber-500">Temporary / page</h1>
             </div>
 
             <div className="flex items-center justify-center space-x-4 ">
-                <button className="bg-green-500 text-white px-4 py-2 rounded"><Link to="/register">Register</Link></button>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded"><Link to="/login">Login</Link></button>
+                <button className="bg-amber-600 text-white px-4 py-2 rounded"><Link to="/register">Register</Link></button>
+                <button className="bg-amber-600 text-white px-4 py-2 rounded"><Link to="/login">Login</Link></button>
             </div>
         </div>
     );
