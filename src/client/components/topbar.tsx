36eed678f71431f8792394967f93e3e5d54f554f
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-04 23:17:34 +0800

    tweak

--- src/client/components/topbar.tsx
+++ src/client/components/topbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Topbar = () => {
     return (
-        <div className="fixed top-0 left-0 right-0 h-12 bg-gray-900 border-b border-gray-600 flex items-center px-4 shadow z-10">
+        <div className="fixed top-0 left-0 right-0 h-12 bg-gray-900 border-b border-slate-700 flex items-center px-4 shadow z-10">
             <span className="text-amber-500 font-bold text-lg">LINAW</span>
         </div>
     );
