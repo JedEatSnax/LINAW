29fde39e1b75018c14c8bf039a18ef5af6c00e53
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-05 02:56:34 +0800

    Migration complete

---
+++ app/client/src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import { Sidebar } from "../components/sidebar";
+import { Homedash } from "../components/homedash";
+import { Topbar } from "../components/topbar";
+
+export function Dashboard() {
+    return(
+        <main>
+            <Topbar />
+            <Sidebar />
+            <Homedash />
+        </main>
+    );
+}
+
+export default Dashboard
