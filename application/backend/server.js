11f2997fb53424c4785985efb0c0af70c606cff1
Wiii-1 <markjunemarino@gmail.com>
2026-03-09 04:41:24 +0800

    setup local postgres docker for more details please refer to docs/progres_report.md

--- application/backend/server.js
+++ application/backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 
 const express = require("express");
 //const { initializeApp, applicationDefault } = require('firebase-admin/app')
+const { Sequelize } = require('sequelize')
 
 const app = express()
 const port = '3000'
@@ -11,6 +12,17 @@ initializeApp({
     credential: applicationDefault()
 });
 */
+
+// Don't delete this. It's the import of the modules for the db and all
+const { router: usersRouter } = require('./routes/users.route');
+const { router: networkRouter } = require('./routes/network.route') 
+
+
+const sequelize = new Sequelize('postgres','linawAdmin@pgAdmin.com', 'linawAdmin',{
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres'
+})
 
 /*
 const cors = require("cors")
@@ -28,6 +40,15 @@ app.use(express.json())
 app.use('/api', usersRouter);
 app.use('/api', networkRouter)
 
-app.listen(port, () => {
-    console.log(`Express running on port ${port}`)
-})
+sequelize.authenticate()
+    .then(() => 
+        console.log('Connected to Postgres SQL (Docker)'),
+    
+    sequelize.sync()
+        .then(() => {
+            app.listen(port, () => {
+                console.log(`Express running on port ${port}`)
+            })
+        })
+)
+
