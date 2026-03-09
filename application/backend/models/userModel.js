f531a0950d1371cbae795c3fd359a9e06b4c3fdc
Wiii-1 <markjunemarino@gmail.com>
2026-03-09 18:11:47 +0800

    some minor changes and remove the hardcoded password and user

---
+++ application/backend/models/userModel.js
@@ -0,0 +1,51 @@
+const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const path = require('path');
+require ('dotenv').config({
+  path: path.resolve(__dirname, '../../.env')
+})
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+async function createUser({ email, password }) {
+  const password_hash = await bcrypt.hash(password, 12);
+
+  try {
+    const r = await pool.query(
+      `insert into users (email, password_hash)
+       values ($1, $2)
+       returning id, email, created_at`,
+      [email, password_hash]
+    );
+    return r.rows[0];
+  } catch (e) {
+    if (e.code === "23505") throw new Error("EMAIL_ALREADY_EXISTS");
+    throw e;
+  }
+}
+
+async function findUserByEmail(email) {
+  const result = await pool.query(
+    "SELECT id, email, password_hash FROM users WHERE email = $1 LIMIT 1",
+    [email]
+  );
+  return result.rows[0] || null;
+}
+
+async function login({ email, password }) {
+  const r = await pool.query(
+    "select id, email, password_hash from users where email = $1 limit 1",
+    [email]
+  );
+  const user = r.rows[0];
+  if (!user) throw new Error("INVALID_CREDENTIALS");
+
+  const ok = await bcrypt.compare(password, user.password_hash);
+  if (!ok) throw new Error("INVALID_CREDENTIALS");
+
+  return { id: user.id, email: user.email };
+}
+
+module.exports = { pool, createUser, findUserByEmail, login };
