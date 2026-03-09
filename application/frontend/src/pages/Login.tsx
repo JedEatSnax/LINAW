742bf43e82051879cdd1d2103303b5dbdeca8743
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-10 02:19:59 +0800

    /login and /register now connected to Firebase

--- application/frontend/src/pages/Login.tsx
+++ application/frontend/src/pages/Login.tsx
@@ -1,45 +1,53 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
 import { useState } from "react";
-import axios from "axios";
-export function Login({setUser}: any) {
-    const [formData, setFormData] = useState({
-        username: "",
-        password: "",
-    });
 
+export function Login() {
+
+    const auth = getAuth();
+    const navigate = useNavigate();
+
+    const [authorizing, setAuthorizing] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({...formData, [e.target.name]: e.target.value });
-    };
+    const signInEmail = async () => {
+        setAuthorizing(true);
+        setError("");
 
-    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("/api/v1/auth/login", formData);
-            localStorage.setItem("token", response.data.token);
-            setUser(response.data.user);
-        } catch (err) {
-            setError("Login failed. Please check your credentials and try again.");
-        }
+        signInWithEmailAndPassword(auth, email, password)
+            .then(response => {
+                console.log("Signed in with email and password:", response.user.uid);
+                navigate("/dashboard");
+            })
+            .catch((error) => {
+                setError(error.message || "Failed to log in");
+                console.error("Error signing in with email and password:", error);
+                setAuthorizing(false);
+            });
     }
 
     return (
             <div className="text-3xl flex items-center justify-center h-screen bg-zinc-950">
                 <div className="flex flex-col items-center justify-center">
-                    <form className="bg-zinc-900 font-mono p-4 rounded shadow-md w-80" onSubmit={handleSubmit}>
+                    <div className="bg-zinc-800 font-mono p-4 rounded shadow-md w-80" >
                         <div className="flex items-center justify-center">
                             <h1 className="text-2xl font-bold mb-4 text-amber-400">Login</h1>
-                            {error && <p className="text-red-500">{error}</p>}
                         </div>
+                        {error && (
+                            <div className="p-4 w-full bg-zinc-700 rounded mb-4">
+                                <p className="text-red-500 text-sm ml-2">{error}</p>
+                            </div>
+                        )}
                         
                         <div>
-                            <input type="text" 
-                            placeholder="Username" 
+                            <input type="email" 
+                            placeholder="E-mail" 
                             className="text-lg mb-2 p-2 border border-gray-300 rounded text-gray-400 w-full" 
-                            name="username"
-                            value={formData.username}
-                            onChange={handleInputChange}
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             required
                             />
                         </div>
@@ -49,8 +57,8 @@ export function Login({setUser}: any) {
                             placeholder="Password" 
                             className="text-lg mb-2 p-2 border border-gray-300 rounded text-gray-400 w-full" 
                             name="password"
-                            value={formData.password}
-                            onChange={handleInputChange} 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             required
                             />
                         </div>
@@ -58,7 +66,8 @@ export function Login({setUser}: any) {
                         <div>
                             <button className="bg-amber-600 hover:bg-amber-700 text-white text-lg px-4 py-2 rounded w-full" 
                             name="loginButton"
-                            type="submit"
+                            onClick={signInEmail}
+                            disabled={authorizing}
                             >Login</button>
                         </div>
 
@@ -66,7 +75,7 @@ export function Login({setUser}: any) {
                             <a href="#" className="text-amber-300 text-sm">Forgot password?</a> 
                             <span className="text-gray-300 text-sm">Don't have an account? <Link to="/register" className="text-amber-300 text-sm">Register here</Link> </span>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
     );
