dc065a316d4bb1f157a8b3d721b1cf007b9870b3
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-10 01:08:13 +0800

    Reverting c833850 because hamburgers are too difficult for me

---
+++ application/frontend/src/components/sidebar.tsx
@@ -0,0 +1,15 @@
+"use-client";
+
+export const Sidebar = () => {
+    return (
+        <div className="fixed h-screen translate-y-12 w-64 bg-gray-800 p-4 shadow border-r border-slate-700">
+            <div className="text-amber-500 font-bold">
+                sidebar
+            </div>
+        </div>
+      </div>
+
+
+    </nav>
+  )
+}
