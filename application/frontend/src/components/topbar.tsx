13b6c8abd021f39ba40cbc97dfbf5ad14c798f55
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-10 01:32:43 +0800

    Revised the component colors and font styles for consistency

--- application/frontend/src/components/topbar.tsx
+++ application/frontend/src/components/topbar.tsx
@@ -2,7 +2,7 @@
 
 export const Topbar = () => {
     return (
-        <div className="fixed top-0 left-0 right-0 h-12 bg-gray-900 border-b border-slate-700 flex items-center px-4 shadow z-10">
+        <div className="fixed top-0 left-0 right-0 h-16 bg-gray-950 border-b border-gray-700 flex items-center px-4 shadow z-10">
             <span className="text-amber-500 font-bold text-lg">LINAW</span>
         </div>
     );
