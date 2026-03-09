b7c3bd3bac37a738e670c38afcdef735a7e0f2f6
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-10 00:22:12 +0800

    Renamed components to hamburger & header, used IBM fonts in components.

--- application/frontend/src/pages/QueryLedger.tsx
+++ application/frontend/src/pages/QueryLedger.tsx
@@ -1,5 +1,5 @@
-import { Header } from "../components/header";
-import Sidebar from "../components/sidebar";
+import { Sidebar } from "../components/hamburger";
+import { Topbar } from "../components/header";
 
 export function QueryLedger() {
     return(
