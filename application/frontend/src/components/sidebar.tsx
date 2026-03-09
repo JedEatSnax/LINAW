ebe885bee58eb0a6cd64d1b7d586fa2f069ca327
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-10 02:14:49 +0800

    Added temporary header.tsx. Still incomplete.

--- application/frontend/src/components/sidebar.tsx
+++ application/frontend/src/components/sidebar.tsx
@@ -3,7 +3,7 @@
 export const Sidebar = () => {
     return (
         <div className="fixed h-screen translate-y-16 w-64 bg-gray-950 p-4 shadow border-r border-gray-700">
-            <div className="text-amber-500 font-ibm-mono">
+            <div className="text-gray-500 font-ibm-mono">
                 NAVIGATION
             </div>
         </div>
--- application/frontend/src/pages/Dashboard.tsx
+++ application/frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Sidebar } from "../components/sidebar";
 import { Homedash } from "../components/homedash";
 import { Topbar } from "../components/topbar";
+import { Header } from "../components/header";
 
 export function Dashboard() {
     return(
