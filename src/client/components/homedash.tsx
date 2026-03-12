bac58213173c92ad7a1d4acf6d19ea70042ac7e8
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-12 19:25:15 +0800

    Removed src (artifact from failed rebase)

--- src/client/components/homedash.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Homedash = () => {
-    return (
-        <div className="flex flex-col bg-gray-950 pb-4 shadow h-screen">
-                <h1 className="text-4xl font-bold mb-4 text-amber-500">Dashboard</h1>
-        </div>
-    );
-}
