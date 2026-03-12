6e650a6c432b8dab5f738e78b76b7ae9718a139b
Wiii-1 <markjunemarino@gmail.com>
2026-03-12 13:54:47 +0800

    fixed login endpoint

--- application/backend/dao/userDao.js
+++ application/backend/dao/userDao.js
@@ -1,15 +1,12 @@
 const db = require('../db/db')
-const knex = require('../db/knex');
 
 class userDao {
     async signup (userData) {
         try {
-            const { email, username, firebaseUID } = userData;
-            console.log('DAO.signup called with:', { email, username, firebaseUID });
+            const { email, firebaseUID } = userData;
             const [user] = await db('users')
             .insert ({
                 email,
-                username: username,
                 firebaseUID: firebaseUID,
             })
             .returning (['id', 'username']);
@@ -18,16 +15,19 @@ class userDao {
         return user;
         } catch (err) {
             if (err.code == "23505") throw Error("EMAIL_ALREADY_EXISTS")
+            throw err
         }
     }
 
-    async login (email, firebaseUID) {
-        let query = knex('user').where('email', email)
-        if (firebaseUID) query.andWhere('firebaseUID', firebaseUID)
-            .where ({ email, firebaseUID })
+    async login (loginData) {
+        const { email, firebaseUID } = loginData
+
+        const user = await db('users')
+            .where({ email, firebaseUID })
             .select('id', 'email', 'username')
-            .first();
-        return user
+            .first()
+
+        return user || null
     }
 
     // async findUserByEmail(email) {
