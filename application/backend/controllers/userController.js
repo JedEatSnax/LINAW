c65389bdd88f56b0ccb1ba5eec0014a9202345d5
Wiii-1 <markjunemarino@gmail.com>
2026-03-12 13:54:47 +0800

    fixed login endpoint

--- application/backend/controllers/userController.js
+++ application/backend/controllers/userController.js
@@ -13,9 +13,21 @@ class userController {
     async login (req, res) {
         try {
             const user = await userService.login(req.body)
-            res.status(201).json(user)
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Invalid email or firebaseUID',
+                    loggedIn: false,
+                })
+            }
+
+            return res.status(200).json({
+                email: user.email,
+                message: 'Login successful',
+                loggedIn: true,
+            })
         } catch (err){
             console.error(err)
+            return res.status(500).json({ message: 'Internal server error' })
         }
     }
 }
