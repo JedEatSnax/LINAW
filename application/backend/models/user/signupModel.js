34aa3f8b9a99831c1777d2440953a83ffe8a1549
Wiii-1 <markjunemarino@gmail.com>
2026-03-12 13:54:47 +0800

    fixed login endpoint

--- application/backend/models/user/signupModel.js
+++ application/backend/models/user/signupModel.js
@@ -2,7 +2,6 @@ const joi = require('joi')
 
 signupSchema = joi.object({
   email: joi.string().email().required(),
-  username: joi.string().min(3).required(),
   firebaseUID: joi.string().required()
 });
 
