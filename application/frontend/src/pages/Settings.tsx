3b6e47a1225c06d1eb0c4d9a7131a8a5be83f665
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-25 12:51:12 +0000

    styles: added more forms for ledger ops, fixed sidebar issue
    Added Transfer, Update, and Delete forms. It's not final yet because the the blockchain is obviously not connected yet. Also, fixed the sidebar issue where it moves whenever you scroll up or down. Might need a scrollbar for the sidebar, too.

--- application/frontend/src/pages/Settings.tsx
+++ application/frontend/src/pages/Settings.tsx
@@ -3,11 +3,11 @@ import { Header } from "../components/header";
 
 export function Settings() {
     return(
-        <main>
+        <main className="h-screen overflow-hidden flex flex-col">
             <Header />
-            <div className="flex">
+            <div className="flex flex-1 overflow-hidden">
                 <Sidebar />
-                <div className="flex-1 p-6">
+                <div className="flex-1 p-6 overflow-y-auto">
                     <h1 className="font-ibm-mono text-amber-400">Settings</h1>
                 </div>
             </div>
