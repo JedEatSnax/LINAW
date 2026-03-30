d4be9d92065a2b22b733a375129afc3dc756caf1
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-30 12:08:42 +0000

    fix(devcontainer): devcontainer & serverSetup syntax

--- .devcontainer/serverSetup.sh
+++ .devcontainer/serverSetup.sh
@@ -8,11 +8,11 @@ function download_node {
 }
 
 function download_hashicorp_vault {
-    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    wget -qO- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com \
-    $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" \
-    | sudo tee /etc/apt/sources.list.d/hashicorp.list
+https://apt.releases.hashicorp.com \
+$(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" \
+    | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
     sudo apt-get update
     sudo apt-get install -y vault
 }
