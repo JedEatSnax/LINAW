1337aff45401efec10da0c01d39bbb15bf800163
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-31 17:27:32 +0800

    chore(backend): compose boilerplate & fabricConfig
    Added the other Docker compose files from the fabric-samples repository, specifically, the Byzantine Fault Tolerant (BFT) and CouchDB implementation. Both of these files aren't complete yet. Next, I revised the structure of fabricConfig.js to make it more secure.

--- backend/config/fabric/fabricConfig.js
+++ backend/config/fabric/fabricConfig.js
@@ -1,18 +1,31 @@
+const versions = Object.freeze({
+    FABRIC_VERSION: '${FABRIC_VERSION:-2.5.15}',
+    FABRIC_CA_VERSION: '${FABRIC_CA_VERSION:-1.5.17}',
+    CRYPTOGEN: 'Certificate Authorities',
+});
 
-FABRIC_VERSION = "${FABRIC_VERSION:-2.5.15}";
-FABRIC_CA_VERSION = "${FABRIC_CA_VERSION:-1.5.17}";
-CRYPTOGEN = "Certificate Authorities";
-CERT_AUTHORITY_COMPOSE = "~/backend/config/compose/certificate-authority.yml"
+const fabricCompose = Object.freeze({
+    CA_COMPOSE: '~/backend/config/compose/certificate-authority.yml',
+    COUCH_COMPOSE: '~/backend/config/compose/couch-db.yml',
+    BFT_COMOSE: '~/backend/config/compose/byzantine-fault-tolerant.yml',
+});
 
+const fabricPaths = Object.freeze({
+    INSTALL_ROOT: '/usr/local/fabric',
+    BIN_DIR: '/usr/local/fabric/bin',
+    CONFIG_DIR: '/usr/local/fabric/config',
+    ORG_DIR: '/usr/local/fabric/organizations',
+    TMP_DIR: '$(mktemp -d)',
+});
 
-INSTALL_ROOT = "/usr/local/fabric";
-BIN_DIR = "${INSTALL_ROOT}/bin";
-CONFIG_DIR = "${INSTALL_ROOT}/config";
-ORG_DIR = "${INSTALL_ROOT}/organizations"
-TMP_DIR = "$(mktemp -d)";
+const organizationPaths = Object.freeze({
+    PEER_ORG_DIR: `${fabricPaths.ORG_DIR}/peerOrganizations`,
+    ORDERER_ORG_DIR: `${fabricPaths.ORG_DIR}/ordererOrganizations`,
+    CERT_AUTHORITY_DIR: `${fabricPaths.ORG_DIR}/certificate-authorities`,
+});
 
-
-// Unfinished and Incomplete
-PEER_ORG_DIR = "${ORG_DIR}/peerOrganizations"
-ORDERER_ORG_DIR = "${ORG_DIR}/ordererOrganizations"
-CERT_AUTHORITY_DIR = "${ORGANIZATION_DIR}/certificate-authorities"
+module.exports = Object.freeze({
+    versions,
+    fabricPaths,
+    organizationPaths,
+});
