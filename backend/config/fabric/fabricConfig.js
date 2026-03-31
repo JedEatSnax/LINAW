3f3c2b698d7fbaaf60adea08008a01d8df7cf1d0
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-03-31 15:41:55 +0800

    chore(backend): temp dir & env setup
    Updated the frontend and backend packages, read the network.sh bash script logic and implemented a temporary directory and environment setup within the backend/config/fabric directory, added the compose file within the fabric-samples repo (must be revised soon)

--- backend/config/fabric/fabricConfig.js
+++ backend/config/fabric/fabricConfig.js
@@ -1,6 +1,18 @@
-'use strict';
 
-/*
-File that handles the environment variables, paths, ports, and hosts
-related to the local Hyperledger Fabric blockchain network.
-*/
+FABRIC_VERSION = "${FABRIC_VERSION:-2.5.15}";
+FABRIC_CA_VERSION = "${FABRIC_CA_VERSION:-1.5.17}";
+CRYPTOGEN = "Certificate Authorities";
+CERT_AUTHORITY_COMPOSE = "~/backend/config/compose/certificate-authority.yml"
+
+
+INSTALL_ROOT = "/usr/local/fabric";
+BIN_DIR = "${INSTALL_ROOT}/bin";
+CONFIG_DIR = "${INSTALL_ROOT}/config";
+ORG_DIR = "${INSTALL_ROOT}/organizations"
+TMP_DIR = "$(mktemp -d)";
+
+
+// Unfinished and Incomplete
+PEER_ORG_DIR = "${ORG_DIR}/peerOrganizations"
+ORDERER_ORG_DIR = "${ORG_DIR}/ordererOrganizations"
+CERT_AUTHORITY_DIR = "${ORGANIZATION_DIR}/certificate-authorities"
